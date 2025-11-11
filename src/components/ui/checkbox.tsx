58b7f419d3108a430435f302c342e267027527c7
arminwoite-web <armin.woite@googlemail.com>
2025-11-11 20:58:12 +0000

    feat: add Checkbox and Label components for better UI abstraction

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -2,11 +2,22 @@ import * as React from "react";
 
 interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
+  onCheckedChange?: (checked: boolean) => void;
 }
 
-export const Checkbox: React.FC<CheckboxProps> = ({ label, ...props }) => (
-  <label className="flex items-center space-x-2">
-    <input type="checkbox" {...props} />
-    {label && <span>{label}</span>}
-  </label>
-);
+export const Checkbox: React.FC<CheckboxProps> = ({
+  label,
+  onCheckedChange,
+  ...props
+}) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onCheckedChange?.(event.target.checked);
+  };
+
+  return (
+    <label className="flex items-center space-x-2">
+      <input type="checkbox" onChange={handleChange} {...props} />
+      {label && <span>{label}</span>}
+    </label>
+  );
+};
